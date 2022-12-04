0f97b6c51944b59d7f60afc9d73b2cc47d7f532d
lottie-isobel <119061074+lottie-isobel@users.noreply.github.com>
2022-12-04 11:25:27 +0000

    day-time added

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let currentDate = document.querySelector(".day-time");
+currentDate.innerHTML = `${day} ${hours}:${minutes}`;
+
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
