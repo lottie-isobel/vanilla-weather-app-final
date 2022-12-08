1e94a666bc3dc8dcd1ae6f942d94f03562ab9929
lottie-isobel <119061074+lottie-isobel@users.noreply.github.com>
2022-12-08 21:32:40 +0800

    Display forecast data with API

--- src/app.js
+++ src/app.js
@@ -19,8 +19,52 @@ if (minutes < 10) {
   minutes = `0${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 let currentDate = document.querySelector(".day-time");
 currentDate.innerHTML = `${day} ${hours}:${minutes}`;
+
+function displayForecast(response) {
+  console.log(response);
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class = "row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `         <div class = "col-2">
+                <div class = "weather-forecast-day">${formatDay(
+                  forecastDay.time
+                )}</div> 
+                <img src = "${forecastDay.condition.icon_url}" width="60"/>
+                <div class = "weather-forecast-temperature">
+                <span class = "weather-forecast-temperature-max">${Math.round(
+                  forecastDay.temperature.maximum
+                )}°</span>
+                <span class = "weather-forecast-temperature-min">${Math.round(
+                  forecastDay.temperature.minimum
+                )}°</span>
+                </div>
+                </div> `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "20199a933076ca9b99t7obdbf4461c24";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
@@ -36,6 +80,8 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   iconElement.setAttribute("src", response.data.condition.icon_url);
+
+  getForecast(response.data.coordinates);
 }
 
 function search(city) {
